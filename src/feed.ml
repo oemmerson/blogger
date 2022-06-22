5d506046d78cd87450b094ba830c0c6d9f802c19
Guillaume Petiot <guillaume@tarides.com>
2022-06-22 21:44:04 +0100

    Update rss metadata

--- src/feed.ml
+++ src/feed.ml
@@ -1,6 +1,6 @@
 open Yocaml
 
-let domain = "https://blog.osau.re"
+let domain = "https:/callmegi.com"
 let feed_url = into domain "feed.xml"
 
 let articles_to_items articles =
@@ -12,11 +12,11 @@ let articles_to_items articles =
 
 let make ((), articles) =
   Yocaml.Rss.Channel.make
-    ~title:"dinoblog"
+    ~title:"callmegi.com"
     ~link:domain
     ~feed_link:feed_url
-    ~description:"MirageOS and OCaml stuffs"
+    ~description:"Articles about what I learn and what I build"
     ~generator:"yocaml"
-    ~webmaster:"din@osau.re"
+    ~webmaster:"guillaume@tarides.com"
     (articles_to_items articles)
 ;;
